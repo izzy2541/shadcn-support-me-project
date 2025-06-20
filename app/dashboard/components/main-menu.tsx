76a3f4628ffed0da4c72058afba85d26985bf171
izzy2541 <isabel.donnelly@hotmail.com>
2025-06-20 19:08:03 +1000

    Add the missing pages and refactor HTML tags

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -6,10 +6,10 @@ import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
 
 export default function MainMenu() {
     return (
-        <div className="bg-muted overflow-auto p-4 flex flex-col">
-            <div className="border-b dark:border-b-black border-b-zinc-300 pb-4">
+        <nav className="bg-muted overflow-auto p-4 flex flex-col">
+            <header className="border-b dark:border-b-black border-b-zinc-300 pb-4">
                 <MenuTitle />
-            </div>
+            </header>
             <div className="py-4 grow">
                 <MenuItem href="/dashboard">My dashboard</MenuItem>
                 <MenuItem href="/dashboard/teams">Teams</MenuItem>
@@ -17,7 +17,7 @@ export default function MainMenu() {
                 <MenuItem href="/dashboard/account">Account</MenuItem>
                 <MenuItem href="/dashboard/settings">Settings</MenuItem>
             </div>
-            <div className="flex gap-2 items-center">
+            <footer className="flex gap-2 items-center">
                 <Avatar>
                     <AvatarFallback className="bg-pink-300 dark:bg-pink-800">ID</AvatarFallback>
                 </Avatar>
@@ -25,7 +25,7 @@ export default function MainMenu() {
                     Logout
                 </Link>
                 <LightDarkToggle/>
-            </div>
-        </div>
+            </footer>
+        </nav>
    )
 }

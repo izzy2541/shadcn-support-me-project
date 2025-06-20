2038417328c8c593f71eca750013a9398a2592f5
izzy2541 <isabel.donnelly@hotmail.com>
2025-06-20 16:04:40 +1000

    Create the MenuTitle

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import MainMenu from "./components/main-menu";
 //children represents any page that gets rendered for each dashboard route
 export default function DashboardLayout({
     children,
@@ -6,7 +7,7 @@ export default function DashboardLayout({
 }){
 return (
 <div className="grid grid-cols-[250px_1fr] h-screen">
-    <div className="bg-muted overflow-auto p-4">side panel</div>
+   <MainMenu />
     <div className="overflow-auto py-2 px-4">
         <div className="pb-4">
             <h1>Welcome back, Tom!</h1>

5c6539ca9aab9ac6a8d3fd9158da9abafb0b701e
izzy2541 <isabel.donnelly@hotmail.com>
2025-06-21 18:41:16 +1000

    Start adding the card content

--- app/dashboard/components/emplyees-stats.tsx
+++ app/dashboard/components/emplyees-stats.tsx
@@ -1,11 +1,35 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { UserIcon } from "lucide-react";
+import Link from "next/link";
 
-export default function EmployeesStats(){
-    return(
+export default function EmployeesStats() {
+    return (
         <div className="grid lg:grid-cols-3 gap-4">
-            <Card>card 1</Card>
-            <Card>card 2</Card>
-            <Card>card 3</Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-base">Total employees</CardTitle>
+                </CardHeader>
+                <CardContent className="flex justify-between items-center">
+                    <div className="flex gap-2">
+                        <UserIcon />
+                        <div className="text-5xl font-bold">100</div>
+                    </div>
+                    <Button size="sm" asChild>
+                        <Link href="/dashboard/employees">View all</Link>
+                    </Button>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-base">Employees present</CardTitle>
+                </CardHeader>
+            </Card>
+            <Card className="border-pink-300">
+                <CardHeader>
+                    <CardTitle className="text-base">Employee of the month</CardTitle>
+                </CardHeader>
+            </Card>
         </div>
     )
 }

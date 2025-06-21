c56edc6e6a647d45f682ba51ec7b8a935a1baa91
izzy2541 <isabel.donnelly@hotmail.com>
2025-06-21 18:06:14 +1000

    Add the employees stats and the teams stats tabs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Tabs, TabsContent, TabsTrigger, TabsList } from "@/components/ui/tabs";
+
 export default function DashboardPage(){
-    return <div>dashboard page</div>
+    return (
+        <Tabs defaultValue="employees">
+            <TabsList>
+                <TabsTrigger value="employees" className="cursor-pointer">Employee stats</TabsTrigger>
+                <TabsTrigger value="teams" className="cursor-pointer">Team stats</TabsTrigger>
+            </TabsList>
+            <TabsContent value="employees">employees stats view</TabsContent>
+            <TabsContent value="teams">teams stats view</TabsContent>
+        </Tabs>
+    )
 }

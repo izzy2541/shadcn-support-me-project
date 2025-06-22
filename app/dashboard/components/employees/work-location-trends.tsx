6a49139bb6a096f84e47a95b6f7e188dc2d61045
izzy2541 <isabel.donnelly@hotmail.com>
2025-06-23 06:24:14 +1000

    Create the stacked bar chart with recharts 2

--- app/dashboard/components/employees/work-location-trends.tsx
+++ app/dashboard/components/employees/work-location-trends.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ResponsiveContainer } from 'recharts';
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const data = [
     {
@@ -65,5 +65,15 @@ const data = [
   ];
   
 export default function WorkLocationTrends(){
-    return <ResponsiveContainer height={350}></ResponsiveContainer>
+    return <ResponsiveContainer height={350} width="100%">
+        <BarChart data={data}>
+            <XAxis dataKey="name" stroke="#888888" fontSize={12}/>
+            <YAxis stroke="#888888" fontSize={12}/>
+            <Tooltip />
+            <Bar dataKey="office" stackId={1} fill='#ec4899' />
+            {/* making stackId successive would make bars appear side by side rather than stacked
+            border radius values are top left - bottom left  */}
+            <Bar dataKey="wfh" stackId={1} fill='#6b7280' radius={[4, 4, 0, 0]}/>     
+        </BarChart>
+    </ResponsiveContainer>
 }

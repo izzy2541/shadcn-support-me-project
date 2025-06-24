f776566f148a5ed0bb0e6ce9ae5a6de90e2a7cca
izzy2541 <isabel.donnelly@hotmail.com>
2025-06-25 07:09:20 +1000

    add the team leaders content

--- app/dashboard/components/teams/teams-stats.tsx
+++ app/dashboard/components/teams/teams-stats.tsx
@@ -1,13 +1,59 @@
-import { Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { AlertTriangleIcon, BadgeCheckIcon, ListCheckIcon, UserCheck2Icon, UserIcon, UserRoundXIcon, UsersIcon } from "lucide-react";
+import { AlertTriangleIcon, BadgeCheckIcon, ListCheckIcon, StarIcon, UserCheck2Icon, UserIcon, UserRoundXIcon, UsersIcon } from "lucide-react";
 import Link from "next/link";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import cm from "@/public/images/cm.jpg"
+import tf from "@/public/images/tf.jpg"
+import rl from "@/public/images/rl.jpg"
+
 import Image from "next/image";
+import { Tooltip, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { TooltipContent } from "@radix-ui/react-tooltip";
 
+const teamLeaders = [
+    {
+      firstName: "Colin",
+      lastName: "Murray",
+      avatar: cm,
+    },
+    {
+      firstName: "Tom",
+      lastName: "Phillips",
+    },
+    {
+      firstName: "Liam",
+      lastName: "Fuentes",
+    },
+    {
+      firstName: "Tina",
+      lastName: "Fey",
+      avatar: tf,
+    },
+    {
+      firstName: "Katie",
+      lastName: "Johnson",
+    },
+    {
+      firstName: "Tina",
+      lastName: "Jones",
+    },
+    {
+      firstName: "Amy",
+      lastName: "Adams",
+    },
+    {
+      firstName: "Ryan",
+      lastName: "Lopez",
+      avatar: rl,
+    },
+    {
+      firstName: "Jenny",
+      lastName: "Jones",
+    },
+  ];
+  
 export default function TeamsStats() {
-
     return (
         <>
             <div className="grid lg:grid-cols-3 gap-4">
@@ -27,11 +73,32 @@ export default function TeamsStats() {
                 </Card>
                 <Card className="gap-2">
                     <CardHeader>
-                        <CardTitle className="text-base">Team leaders</CardTitle>
+                        <CardTitle className="text-base flex justify-between">
+                            <span>Team leaders</span>
+                            <StarIcon className="text-yellow-500"/>
+                        </CardTitle>
                     </CardHeader>
-                    <CardContent>
-                        <div className="flex gap-2">
-                        </div>
+                    <CardContent className="flex flex-wrap gap-2">
+                        {teamLeaders.map(teamLeader => (
+                            <TooltipProvider key={`${teamLeader.firstName}${teamLeader.lastName}`}>
+                                    <Tooltip>
+                                        <TooltipTrigger asChild>
+                                            <Avatar>
+                                                {!!teamLeader.avatar && 
+                                                <Image src={teamLeader.avatar} alt={`${teamLeader.firstName} ${teamLeader.lastName} avatar`}/>
+                                                }
+                                            <AvatarFallback>
+                                            {teamLeader.firstName[0]}
+                                            {teamLeader.lastName[0]}
+                                            </AvatarFallback>
+                                            </Avatar>
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                        {teamLeader.firstName} {teamLeader.lastName}
+                                        </TooltipContent>
+                                    </Tooltip>
+                            </TooltipProvider>    
+                        ))}
                     </CardContent>
                 </Card>
                 <Card className="gap-2 flex flex-col">
@@ -39,8 +106,8 @@ export default function TeamsStats() {
                         <CardTitle className="text-base">Team distribution</CardTitle>
                     </CardHeader>
                     <CardContent className="flex gap-2 items-center">
-</CardContent>
-        
+                    </CardContent>
+
                 </Card>
             </div>
             <Card className="my-4">
